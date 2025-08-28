6973ab46a2235c3be0790a7db9501eb93f6bacf9
reaperhound <reaperhound69004@gmail.com>
2025-08-28 10:41:31 +0530

    test

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,5 +1,15 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ msg: "Hii" });
+  const rfnum = "108145";
+  const response = await fetch(
+    `https://www.magicbricks.com/mbproject/newProjectCards?&pageNo=1&city=${rfnum}`,
+    {
+      //   headers,
+      body: null,
+      method: "GET",
+    }
+  );
+  const data = await response.json();
+  return NextResponse.json({ msg: "Hii", data });
 }
