8e2508bc06f29c745f7af62f84058390e940a6dd
reaperhound <reaperhound69004@gmail.com>
2025-08-28 10:37:41 +0530

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,20 @@
+"use client";
+
 import SearchBar from "@/components/SearchBar";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {}, []);
+  async function foo() {
+    const res = await fetch("/api/test");
+    const data = await res.json();
+    console.log(data);
+  }
   return (
     <div className='p-6'>
       <SearchBar />
+      <button onClick={foo}>TEST</button>
     </div>
   );
 }
